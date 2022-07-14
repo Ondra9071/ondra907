012af2f5a09fc0887f8374bc942fb02f2356ef9a
Ondra907 <89251608+Ondra9071@users.noreply.github.com>
2022-07-14 13:59:19 +0200

    Add files via upload

---
+++ config1.js
@@ -0,0 +1,37 @@
+let config = {
+    "Title": "Ondra907 | Links",
+    "Name": "Ondra907",
+    "Description": "👦 15yo, 💻 Czech Starting Dev",
+    "links": [
+      {
+        "Title": "Website",
+        "URL": "https://ondra907.xyz/",
+        "icon_classes": "fas fa-link",
+      },
+      {
+        "Title": "Instagram",
+        "URL": "https://instagram.com/ondrasekq_",
+        "icon_classes": "fab fa-instagram fa-page",
+      },
+      {
+        "Title": "Github",
+        "URL": "https://github.com/ondra9071",
+        "icon_classes": "fab fa-github",
+      },
+      {
+        "Title": "Discord",
+        "URL": "https://ondra907.xyz/discord",
+        "icon_classes": "fab fa-discord"
+      },
+      {
+        "Title": "Steam",
+        "URL": "https://steamcommunity.com/id/ondra9077",
+        "icon_classes": "fab fa-steam"
+      },
+      {
+        "Title": "Facebook",
+        "URL": "https://www.facebook.com/ondra9071/",
+        "icon_classes": "fab fa-facebook"
+      },
+    ]
+  }
